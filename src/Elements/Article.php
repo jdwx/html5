16635b01dfe8a23fb53d8421286eafaab3b43023
JDW <xs@jdw.sx>
2020-10-31 20:07:46 -0400

    Add Article tag.

---
+++ src/Elements/Article.php
@@ -0,0 +1,24 @@
+<?php
+
+
+declare( strict_types = 1 );
+
+
+namespace JDWX\HTML5\Elements;
+
+
+use JDWX\HTML5\Element;
+use JDWX\HTML5\IParent;
+
+
+class Article extends Element {
+
+
+    public function __construct( IParent $i_par, ...$i_rxChildren ) {
+        parent::__construct( $i_par, 'article', ... $i_rxChildren );
+    }
+
+
+}
+
+

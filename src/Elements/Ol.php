8306edff307b99eec6e6d13ad2e0bbe6bd082fe1
JDW <xs@jdw.sx>
2020-11-21 13:54:46 -0500

    Add <ol> tag.

---
+++ src/Elements/Ol.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace JDWX\HTML5\Elements;
+
+
+use JDWX\HTML5\IParent;
+
+
+class Ol extends \JDWX\HTML5\Element {
+
+
+    public function __construct( IParent $i_par, ... $i_rxChildren ) {
+        parent::__construct( $i_par, 'ol', ... $i_rxChildren );
+    }
+
+
+}
+
+

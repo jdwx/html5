9d7bff8bac531fc04c2fb6de2106d2ab30aa8da2
JDW <xs@jdw.sx>
2025-05-22 22:17:06 -0400

    Add required to FormChildTrait.

--- src/Traits/FormChildTrait.php
+++ src/Traits/FormChildTrait.php
@@ -10,6 +10,7 @@ namespace JDWX\HTML5\Traits;
 use JDWX\HTML5\Attributes\DisabledTrait;
 use JDWX\HTML5\Attributes\FormTrait;
 use JDWX\HTML5\Attributes\NameTrait;
+use JDWX\HTML5\Attributes\RequiredTrait;
 
 
 trait FormChildTrait {
@@ -18,4 +19,5 @@ trait FormChildTrait {
     use DisabledTrait;
     use FormTrait;
     use NameTrait;
+    use RequiredTrait;
 }
